ffd07bc71352b5a72854d1f44aded4a1c91b9d5a
Stark <33543133+stark87@users.noreply.github.com>
2022-02-21 19:18:24 +0100

    paypal button added

--- src/components/cart/CartTotals.js
+++ src/components/cart/CartTotals.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import PayPalButton from './PayPalButton';
 
-export default function CartTotals({ value }) {
+export default function CartTotals({ value, history }) {
     const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
     return (
         <React.Fragment>
             <div className="container">
                 <div className="row">
-                    <div className="col-10 mt-2 ms-sm-5 ms-md-auto col-sm-8 text-capitalize text-right">
+                    <div className="col pe-5 pe-md-3 mt-2 text-capitalize text-end">
                         <Link to="/">
                             <button className="btn btn-outline-danger text-uppercase mb-3 px-5" type="button" onClick={() => clearCart()}>
                                 clear cart
@@ -31,6 +32,9 @@ export default function CartTotals({ value }) {
                             </span>
                             <strong>$ {cartTotal}</strong>
                         </h5>
+                        <PayPalButton total = {cartTotal} clearCart={clearCart} history={history}>
+
+                        </PayPalButton>
                     </div>
                 </div>
             </div>
--- src/components/cart/EmptyCart.js
+++ src/components/cart/EmptyCart.js
@@ -2,12 +2,10 @@ import React from 'react'
 
 export default function EmptyCart() {
     return (
-        <div className="container mt-5">
-            <div className="row m-0">
-                <div className="col-10 mx-auto text-center text-title">
-                    <h1>your cart is currently empty</h1>
-                </div>
-            </div> 
+        <div className="row m-0">
+            <div className="col-10 mx-auto text-center text-title">
+                <h1>your cart is currently empty</h1>
+            </div>
         </div>
     )
 }
